685b8d593d2a2ca8a362341b866ecf6854e4fe3d
Keith Filkins <kafilkins@me.com>
2020-11-06 01:04:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,11 @@
 def display_board(board)
-
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  separator = "|"
+lines = "-----------"
+puts "#{board[0]} #{separator} #{board[1]} #{separator} #{board[2]}"
+puts "#{lines}"
+puts "#{board[3]} #{separator} #{board[4]} #{seperator} #{board[5]}"
+puts "#{lines}"
+puts "#{board[6]} #{separator} #{board[7]} #{seperator} #{board[8]}"
 
 end
 
